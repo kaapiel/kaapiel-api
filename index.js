4162aeb18c534be557c366804258915f73c57e92
Gabriel Aguido Fraga <gfraga@stack8.com>
2020-08-04 21:17:57 -0400

    qalenium

--- index.js
+++ index.js
@@ -8,5 +8,21 @@ app.listen(PORT, () => {
 });
 
 app.get("/owner", (req, res, next) => {
-    res.json({"name":"Gabriel Aguido Fraga"});
+    res.json(
+        {"name":"Gabriel Aguido Fraga",
+            "gitHub":"https://github.com/kaapiel",
+            "linkedIn":"https://www.linkedin.com/in/gabriel-aguido-fraga/",
+            "instagram":"https://www.instagram.com/gabrielaguidofraga/"
+        }
+    );
+});
+
+app.get("/qalenium", (req, res, next) => {
+    res.json(
+        {"community":"QAlenium",
+            "gitHub":"https://github.com/QAlenium",
+            "linkedIn":"https://www.linkedin.com/company/qalenium",
+            "instagram":"to be created"
+        }
+    );
 });
