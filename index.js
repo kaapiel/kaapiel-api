bccb7b7eb1565175919ab7b58fbd1298f78b3f7c
Gabriel Aguido Fraga <gfraga@stack8.com>
2020-08-04 21:35:43 -0400

    added phone attribute

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+const request = require('request');
 var express = require("express");
 var app = express();
 
 const PORT = process.env.PORT || 8147;
+const QALENEIUM_GROUPID = "-1001241726927L";
+const QALENEIUM_BOT = "1339813179:AAH6Y3bKGhSnafpzTdiDK7Yq-hYhVbdPsKU";
 
 app.listen(PORT, () => {
     console.log("Server running on port ${PORT}}");
@@ -26,5 +29,47 @@ app.get("/qalenium", (req, res, next) => {
             "instagram":"to be created",
             "phoneNumber":"to be created"
         }
+    );
+});
+
+app.get("/circleCI", (req, res, next) => {
+    res.json(
+        {}
+    );
+});
+
+app.get("/sonarcloud", (req, res, next) => {
+    res.json(
+        {}
+    );
+});
+
+app.get("/slack", (req, res, next) => {
+    res.json(
+        {}
+    );
+});
+
+app.get("/blazemeter", (req, res, next) => {
+    res.json(
+        {}
+    );
+});
+
+app.get("/docker", (req, res, next) => {
+    res.json(
+        {}
+    );
+});
+
+app.get("/twitter", (req, res, next) => {
+    res.json(
+        {}
+    );
+});
+
+app.get("/instagram", (req, res, next) => {
+    res.json(
+        {}
     );
 });
